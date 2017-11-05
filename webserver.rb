caa4cde9b27763bc128232195f8e4f8b05f54786
bransynluther <bransynluther@gmail.com>
2017-11-05 06:31:00 -0500

    update apache server services

--- webserver.rb
+++ webserver.rb
@@ -1,6 +1,13 @@
 apt_update 'Update the apt cache daily' do
   frequency 86_400
-  action: periodic
+  action :periodic
 end
 
 package 'apache2'
+
+
+service'apache2' do
+  supports status: true
+  action[:enable, start]
+  action [:enable, start
+
