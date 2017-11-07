984132e76f225ff7619da6d3e345d8b0e0d81814
bransynluther <bransynluther@gmail.com>
2017-11-06 22:16:34 -0500

    rename /cookbook to /cookbooks fix error in recipe

--- webserver.rb
+++ webserver.rb
@@ -10,10 +10,6 @@ service 'apache2' do
   action [:enable, :start]
 end
 
-file '/var/www/html/index.html' do
-  content '<html>
-  <body>
-  <h1> hello world </h1>
-  </body>
-</html>'
+template 'var/www/html/index.html' do
+  source 'index.html.erb'
 end
